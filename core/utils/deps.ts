8d604ce4de30ee292704dd37b08a8db8188aee5c
buttercubz <ericksosagarcias@gmail.com>
2021-06-10 14:32:29 -0400

    chore: update dependencies

--- core/utils/deps.ts
+++ core/utils/deps.ts
@@ -1,13 +1,13 @@
 // deno std library
-export * as fs from "https://deno.land/std@0.83.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.83.0/path/mod.ts";
-export * as colors from "https://deno.land/std@0.83.0/fmt/colors.ts";
-export * as http from "https://deno.land/std@0.83.0/http/mod.ts";
-export { v4 } from "https://deno.land/std@0.88.0/uuid/mod.ts";
+export * as fs from "https://deno.land/std@0.98.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.98.0/path/mod.ts";
+export * as colors from "https://deno.land/std@0.98.0/fmt/colors.ts";
+export * as http from "https://deno.land/std@0.98.0/http/mod.ts";
+export { v4 } from "https://deno.land/std@0.98.0/uuid/mod.ts";
 export {
   assertEquals,
   assertNotEquals,
-} from "https://deno.land/std@0.83.0/testing/asserts.ts";
+} from "https://deno.land/std@0.98.0/testing/asserts.ts";
 import { WebSocketClient, WebSocketServer } from "https://deno.land/x/websocket@v0.1.1/mod.ts";
 export { WebSocketServer };
 export type { WebSocketClient };
@@ -18,7 +18,7 @@ export {
   Context,
   Router,
   send,
-} from "https://deno.land/x/oak@v6.5.0/mod.ts";
+} from "https://deno.land/x/oak@v7.5.0/mod.ts";
 export { superoak } from "https://deno.land/x/superoak@4.0.0/mod.ts";
 
 // third-party
@@ -32,5 +32,5 @@ export { compile as scssCompiler } from "https://raw.githubusercontent.com/crewd
 export * as sfcCompiler from "https://denopkg.com/crewdevio/vue-deno-compiler/mod.ts";
 
 //event emitter for live reload
-import { EventEmitter } from "https://deno.land/std@0.93.0/node/events.ts";
+import { EventEmitter } from "https://deno.land/std@0.98.0/node/events.ts";
 export { EventEmitter };
